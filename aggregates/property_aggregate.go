e00be8e19ce3b1a0739d268f70c0061008535f2f
Andres Colonia <andreskal@gmail.com>
2024-06-07 19:33:15 -0400

    feat: create basic 1-level aggregate to persist in storage

---
+++ aggregates/property_aggregate.go
@@ -0,0 +1,35 @@
+package aggregates
+
+import (
+	"errors"
+
+	"github.com/kalelc/event-sourcing-with-golang/events"
+)
+
+type Property struct {
+	ID            int64
+	PropertyTaxID string
+}
+
+func (p *Property) Apply(event events.Event) error {
+	switch e := event.(type) {
+	case events.PropertyCreated:
+		p.ID = e.ID
+		p.PropertyTaxID = e.PropertyTaxID
+	default:
+		return errors.New("unknown event type")
+	}
+	return nil
+}
+
+func (p *Property) CreateProperty(id int64, propertyTaxID string) ([]events.Event, error) {
+	if p.ID != 0 {
+		return nil, errors.New("user already exists")
+	}
+	event := events.NewPropertyCreated(id, propertyTaxID)
+	err := p.Apply(event)
+	if err != nil {
+		return nil, err
+	}
+	return []events.Event{event}, nil
+}
